010211f923a513a89f2969378d32a98170999fcc
Rizky Haryadi <haryadi.rizky@ymail.com>
2017-11-03 13:21:20 +0700

    1.0

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next)
+{
+	res.render('login',{ title:'Login'})
+});
+
+router.get('/profile', function(req, res, next)
+{	
+	res.render('profile',{ title:'Profile'})
+}	);
+
+module.exports = router;
